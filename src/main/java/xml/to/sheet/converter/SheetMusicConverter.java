8dcc39acb672ff3e02d9a8de5784b7570fa68780
Parth Sharma <psharm09@my.yorku.ca>
2022-02-05 19:22:41 -0500

    Merge branch 'Feature-Parth&Kedi' of https://github.com/partsharma99/TAB2XML into Feature-Parth&Kedi

--- src/main/java/xml/to/sheet/converter/SheetMusicConverter.java
+++ src/main/java/xml/to/sheet/converter/SheetMusicConverter.java
@@ -9,12 +9,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import utility.MusicXMLCreator;
 import GUI.*;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class SheetMusicConverter {
 	
 	private Score score;
 	private MusicXMLCreator mxlc;
 	private MainViewController mvc;
-	
+	private static final String FILENAME = null;
 	public SheetMusicConverter(MainViewController mvc, Score score) {
 		this.mvc = mvc;
 		this.score = score;
@@ -40,5 +55,30 @@ public class SheetMusicConverter {
 		fw.write(xml);
 		fw.close();
 		
+		 DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		 try {
+			 dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+
+	          // parse XML file
+	          DocumentBuilder db = dbf.newDocumentBuilder();
+
+	          Document doc = db.parse(new File("my-file.xml"));
+	          NodeList measurelist = doc.getElementsByTagName("measure");
+	          
+	          for (int i = 0; i < measurelist.getLength(); i++) {
+
+	              Node node = measurelist.item(i);
+	              
+	              if (node.getNodeType() == Node.ELEMENT_NODE) {
+	            	  
+	              }
+	              
+	          }
+
+		 }
+		 catch (ParserConfigurationException | SAXException | IOException e) {
+	          e.printStackTrace();
+		  
 		}
+}
 }
