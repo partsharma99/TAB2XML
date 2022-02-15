d1b7069bd0d046a268df81a9d371fe8988f8d83c
Mohammad Bilal Khan <mohamk29@my.yorku.ca>
2022-02-14 19:40:18 -0500

    Created the previewButton GUI through FXML
    Was initially done in Swing. Change was done in order to continue the
    design flow of the app. Required buttons have also been added.

---
+++ src/main/java/GUI/PreviewMXLController.java
@@ -0,0 +1,14 @@
+package GUI;
+
+public class PreviewMXLController {
+
+	public void setMainViewController(MainViewController mvcInput) {
+    }
+	
+	public void savePDF() {
+	}
+	
+	public void handleGotoMeasure() {
+	}
+	
+}
