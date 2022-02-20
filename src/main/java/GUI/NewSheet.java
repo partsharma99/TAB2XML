6fce6fc99c4d1f45b1f21a4da6ec77b51ef6b1e4
sina1 <sina1@Sina>
2022-02-20 01:23:39 -0500

    made a note info class where we get information on notes to draw. also made a NOTE amount class where it returns how many notes we have to be used in the newsheet class to draw the notes.

--- src/main/java/GUI/NewSheet.java
+++ src/main/java/GUI/NewSheet.java
@@ -4,6 +4,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -26,36 +27,75 @@ public class NewSheet extends MainViewController {
 		JFrame frame = new JFrame();
 		JLabel label=new JLabel();
 		//private MusicXMLCreator mxlc;
-		
+		int x;
+		int y;
+		int framex=420;
+		int framey=420;
 		
 		public NewSheet(String xml){
 			XmlConverter nn = new XmlConverter();
-			try {
-				JAXBContext jc = JAXBContext.newInstance(XmlConverter.class);
-
-		        Unmarshaller unmarshaller = jc.createUnmarshaller();
-		        nn = (XmlConverter) unmarshaller.unmarshal(new StringReader(xml));
-		        }
-				catch (JAXBException e) 
-				{
-				  e.printStackTrace();
-				}
+//			try {
+//				JAXBContext jc = JAXBContext.newInstance(XmlConverter.class);
+//
+//		        Unmarshaller unmarshaller = jc.createUnmarshaller();
+//		        nn = (XmlConverter) unmarshaller.unmarshal(new StringReader(xml));
+//		        }
+//				catch (JAXBException e) 
+//				{
+//				  e.printStackTrace();
+//				}
 			
 			frame.setContentPane(new DrawPane());
+			
 			JLabel label= new JLabel(InstrumentType.getInstrumentType(xml));
 			JLabel label2= new JLabel(String.valueOf(Lines.getLines(xml)));
 			JLabel label3 = new JLabel(nn.getSign());
+			
+			ArrayList<Integer> yaxis =NoteInfo.GgetY(xml);
+			ArrayList<Integer> xaxis =NoteInfo.GgetX(xml);
+			ArrayList<Integer> fret =NoteInfo.Ggetfret(xml);
+
+			//loop will tun to the amount of notes found in xml to draw each note
+			
+			System.out.print(Noteamount.getSize(xml));
+			NoteInfo.GgetY(xml);
+
+			
+			
+			
+			
 			label.setBounds(0,200,100,50);
 			label.setFont(new Font(null,Font.PLAIN,25));
 			label2.setBounds(0,240,100,50);
 			label2.setFont(new Font(null,Font.PLAIN,25));
 			label3.setBounds(0,270,100,50);
 			label3.setFont(new Font(null,Font.PLAIN,25));
+for(int i=0;i<Noteamount.getSize(xml);i++) {
+				
+				JLabel note = new JLabel(String.valueOf(NoteInfo.Ggetfret(xml).get(i)));
+				
+				// x= note line
+				// y=note timing
+				
+
+				y=20+(yaxis.get(i)-1)*30;
+				x=20+(xaxis.get(i)-1)*30;
+				
+				
+				note.setBounds(x, y, x+5, y+5);
+				frame.add(note);
+				
+				//extending the frame
+				//framex=framex+10;
+				//framey=framey+10;
+				System.out.println(yaxis);
+				
+			}
 			frame.add(label);
 			frame.add(label2);
 			frame.add(label3);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setSize(420,420);
+			frame.setSize(framex,framey);
 			frame.setLayout(null);
 			frame.setVisible(true);
 //			
---
+++ src/main/java/GUI/NoteInfo.java
@@ -0,0 +1,70 @@
+package GUI;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NoteInfo {
+
+	 private static int lines;
+	 static int max=0;
+	static int i = 1;
+	static ArrayList<Integer> x = new ArrayList<Integer>();
+	static ArrayList<Integer> y = new ArrayList<Integer>();
+	static ArrayList<Integer> f = new ArrayList<Integer>();
+
+
+	//for guitar
+	public static ArrayList<Integer> GgetY(String sml) {
+		
+		
+        Pattern pattern = Pattern.compile("<string>(.*?)</string>");
+        Matcher matcher = pattern.matcher(sml);
+        while (matcher.find()) {
+        	//x.add(i);
+        	y.add(Integer. parseInt(matcher.group(1)));
+        	i++;
+        }
+
+		
+		return y;
+		
+		
+	}
+	
+public static ArrayList<Integer> GgetX(String sml) {
+		
+		
+        Pattern pattern = Pattern.compile("<string>(.*?)</string>");
+        Matcher matcher = pattern.matcher(sml);
+        while (matcher.find()) {
+        	x.add(i);
+        	//y.add(matcher.group(1));
+        	i++;
+        }
+
+		
+		
+		return x;
+		
+		
+	}
+public static ArrayList<Integer> Ggetfret(String sml) {
+	
+	
+    Pattern pattern = Pattern.compile("<fret>(.*?)</fret>");
+    Matcher matcher = pattern.matcher(sml);
+    while (matcher.find()) {
+    	f.add(Integer. parseInt(matcher.group(1)));
+    	//y.add(matcher.group(1));
+    	i++;
+    }
+
+	
+	
+	return f;
+	
+	
+}
+	
+}
