c81dbb3583ec50cae585c9a2f5f25b067cdbac00
Mohammad Bilal Khan <mohamk29@my.yorku.ca>
2022-03-03 17:46:48 -0500

    Cleaned up code

--- src/main/java/GUI/PreviewMXLController.java
+++ src/main/java/GUI/PreviewMXLController.java
@@ -1,24 +1,14 @@
 package GUI;
-
-//import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-//import javafx.fxml.Initializable;
-//
-//import javafx.scene.*;
 import javafx.scene.canvas.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
-//import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import xml.to.sheet.converter.ListOfMeasureAndNote;
 import xml.to.sheet.converter.POJOClasses.Note2;
-//import javafx.scene.paint.*;
-//import javafx.scene.text.Font;
-//import javafx.stage.Stage;
-//import javafx.stage.Window;
 import xml.to.sheet.converter.POJOClasses.ScorePartwise2;
 import xml.to.sheet.converter.POJOClasses.XmlToJava;
 
@@ -27,31 +17,12 @@ import java.util.List;
 
 import javax.swing.JLabel;
 import javax.xml.bind.JAXBException;
-//import java.net.URL;
-//import java.util.ResourceBundle;
-//
-//import org.fxmisc.flowless.VirtualizedScrollPane;
-//import org.fxmisc.richtext.CodeArea;
-//import org.fxmisc.richtext.LineNumberFactory;
-//
-//import converter.Converter;
-/*
-Sample tab
-|-----------0-----|-0---------------|
-|---------0---0---|-0---------------|
-|-------1-------1-|-1---------------|
-|-----2-----------|-2---------------|
-|---2-------------|-2---------------|
-|-0---------------|-0---------------|
-
-*/
 public class PreviewMXLController {
 	
 	@FXML public Canvas canvas;
 	@FXML TextField gotoMeasureField;
 	@FXML Button gotoMeasureButton;
 	@FXML Button savePDF;
-//	private GraphicsContext gc;
 	public FXMLLoader loader;
 	
 	@FXML
@@ -62,42 +33,6 @@ public class PreviewMXLController {
 	public void handleGotoMeasure() {
 	}
 	
-//	public void drawLines() {
-//		gc = canvas.getGraphicsContext2D();
-//        gc.setFill(Color.WHITE);
-//        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-//        System.out.println("color set to white and background rectangle drawn");
-//        
-//        gc.strokeLine(10, 100, 200, 100);
-//        gc.strokeLine(10, 110, 200, 110);
-//        gc.strokeLine(10, 120, 200, 120);
-//        gc.strokeLine(10, 130, 200, 130);
-//        gc.strokeLine(10, 140, 200, 140);
-//        System.out.println("drawn lines");
-//	}
-//	
-//	public void drawTestRectangle() {
-//		gc = canvas.getGraphicsContext2D();
-//        gc.setFill(Color.BLACK);
-//        System.out.println("color set to black");
-//        gc.fillRect(50, 50, 100, 100);
-//        System.out.println("draw rectangle");
-//	}
-//	// paint the canvas
-//	public void paint(GraphicsContext g) {
-//		// set color to red
-//		//g.setColor(Color.BLACK);
-//		g.setFill(Color.ALICEBLUE);
-//
-//		// set Font
-//		g.setFont(new Font("Bold", 1));
-//		g.lineTo(100, 100);
-//
-//	}
-//
-//	@Override
-//	public void start(Stage primaryStage) throws Exception {
-//	}
 	
     private MainViewController mvc;
 	@FXML 
