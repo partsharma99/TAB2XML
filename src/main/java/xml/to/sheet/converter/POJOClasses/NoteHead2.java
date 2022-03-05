c2f85768f39eaa02249ddef1a154d7bd6c59c1b7
Kedamawi Mengistu <Kedim22@my.yorku.ca>
2022-03-05 14:59:50 -0500

    adding to parser

--- src/main/java/xml/to/sheet/converter/POJOClasses/NoteHead2.java
+++ src/main/java/xml/to/sheet/converter/POJOClasses/NoteHead2.java
@@ -1,5 +1,37 @@
 package xml.to.sheet.converter.POJOClasses;
 
 public class NoteHead2 {
+	
+	private String parantheses;
+	private String text;
 
+	public NoteHead2() {
+	}
+
+	public NoteHead2(String parantheses, String text) {
+		this.parantheses = parantheses;
+		this.text = text;
+	}
+
+	public String getParantheses() {
+		return parantheses;
+	}
+
+	public void setParantheses(String parantheses) {
+		this.parantheses = parantheses;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public String toString() {
+		return "NoteHead2 [parantheses=" + parantheses + ", text=" + text + "]";
+	}
+	
 }
