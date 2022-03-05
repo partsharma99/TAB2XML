877eedbaeac349cc17f15bde52178d1b63ba017d
Kedamawi Mengistu <Kedim22@my.yorku.ca>
2022-03-05 14:51:53 -0500

    adding to parser

--- src/main/java/xml/to/sheet/converter/POJOClasses/Tremelo2.java
+++ src/main/java/xml/to/sheet/converter/POJOClasses/Tremelo2.java
@@ -1,5 +1,43 @@
 package xml.to.sheet.converter.POJOClasses;
 
-public class Tremelo2 {
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 
+public class Tremelo2 {
+	
+	private String type;
+	private int text;
+
+	public Tremelo2() {
+	}
+	
+	public Tremelo2(String type, int text) {
+		this.type = type;
+		this.text = text;
+	}
+	
+	@XmlAttribute
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	@XmlElement
+	public int getText() {
+		return text;
+	}
+
+	public void setText(int text) {
+		this.text = text;
+	}
+
+	@Override
+	public String toString() {
+		return "Tremelo [type=" + type + ", text=" + text + "]";
+	}
+	
 }
+
