d0fcca4ab186beef498d4e86acc506054658c551
Parth Sharma <psharm09@my.yorku.ca>
2022-03-06 23:53:37 -0500

    Midterm submission

--- src/main/java/GUI/PreviewMXLController.java
+++ src/main/java/GUI/PreviewMXLController.java
@@ -1,12 +1,7 @@
 
-
 package GUI;
 
-
-
-import java.io.IOException;
-
-
+//import javafx.application.Application;
 import javafx.fxml.FXML;
 
 import javafx.fxml.FXMLLoader;
@@ -30,22 +25,19 @@ import xml.to.sheet.converter.POJOClasses.Note2;
 import xml.to.sheet.converter.POJOClasses.ScorePartwise2;
 import xml.to.sheet.converter.POJOClasses.XmlToJava;
 
-import javafx.fxml.FXML;
-import javafx.scene.layout.Pane;
 import java.io.IOException;
 import java.util.List;
 
 import javax.swing.JLabel;
 import javax.xml.bind.JAXBException;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import org.fxmisc.flowless.VirtualizedScrollPane;
-import org.fxmisc.richtext.CodeArea;
-import org.fxmisc.richtext.LineNumberFactory;
-
-import converter.Converter;
+//import java.net.URL;
+//import java.util.ResourceBundle;
+//
+//import org.fxmisc.flowless.VirtualizedScrollPane;
+//import org.fxmisc.richtext.CodeArea;
+//import org.fxmisc.richtext.LineNumberFactory;
+//
+//import converter.Converter;
 /*
 Sample tab
 |-----------0-----|-0---------------|
@@ -56,17 +48,13 @@ Sample tab
 |-0---------------|-0---------------|
 
 */
-
 public class PreviewMXLController {
-
 	
-//	@FXML public Canvas canvas;
+	@FXML public Canvas canvas;
 	@FXML TextField gotoMeasureField;
 	@FXML Button gotoMeasureButton;
 	@FXML Button savePDF;
-
-	private GraphicsContext gc;
-
+//	private GraphicsContext gc;
 	public FXMLLoader loader;
 	
 	@FXML
@@ -76,9 +64,43 @@ public class PreviewMXLController {
 	@FXML
 	public void handleGotoMeasure() {
 	}
-
-
-
+	
+//	public void drawLines() {
+//		gc = canvas.getGraphicsContext2D();
+//        gc.setFill(Color.WHITE);
+//        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+//        System.out.println("color set to white and background rectangle drawn");
+//        
+//        gc.strokeLine(10, 100, 200, 100);
+//        gc.strokeLine(10, 110, 200, 110);
+//        gc.strokeLine(10, 120, 200, 120);
+//        gc.strokeLine(10, 130, 200, 130);
+//        gc.strokeLine(10, 140, 200, 140);
+//        System.out.println("drawn lines");
+//	}
+//	
+//	public void drawTestRectangle() {
+//		gc = canvas.getGraphicsContext2D();
+//        gc.setFill(Color.BLACK);
+//        System.out.println("color set to black");
+//        gc.fillRect(50, 50, 100, 100);
+//        System.out.println("draw rectangle");
+//	}
+	// paint the canvas
+//	public void paint(GraphicsContext g) {
+//		// set color to red
+//		//g.setColor(Color.BLACK);
+//		g.setFill(Color.ALICEBLUE);
+//
+//		// set Font
+//		g.setFont(new Font("Bold", 1));
+//		g.lineTo(100, 100);
+//
+//	}
+//
+//	@Override
+//	public void start(Stage primaryStage) throws Exception {
+//	}
 	
     private MainViewController mvc;
 	@FXML 
@@ -99,7 +121,7 @@ public class PreviewMXLController {
             	y += 13;
         	}
      	}
-     	else if (instrument.equalsIgnoreCase("Drumset")) {
+     	else if (instrument.equalsIgnoreCase("drumset")) {
      		int i = 1;
      		while (i <= 5) {
              	DrawLine dl = new DrawLine(0.0, y, this.pane.getMaxWidth(), y);
@@ -174,15 +196,7 @@ public class PreviewMXLController {
     public void drawNotes(double x, double y,String a,int f) {
     	String num = a;
     	 Text t = new Text(x, y, a);
-
          t.setFont(Font.font("calibri", f));
-
-
-         t.setFont(Font.font("veranda", 14));
-
-         t.setFont(Font.font("calibri", f));
-
-
          pane.getChildren().add(t);
     	
     }
@@ -216,81 +230,48 @@ public class PreviewMXLController {
     	ScorePartwise2 sc;
 		try {
 			sc = XmlToJava.unmarshal(mvc.converter.getMusicXML(), ScorePartwise2.class);
+			// int numMeasures = ListOfMeasureAndNote.getlistOfMeasures(sc).size();
 			 List<Note2> notes = ListOfMeasureAndNote.getlistOfNotes(sc);
-		     String instName = sc.getInstrumentName();
-		     String cleff = sc.getCleff();
+		     String instName = sc.getPartlist().getScorepart().get(0).getPartname();
+		     String cleff = sc.getListOfParts().get(0).getListOfMeasures().get(0).getAttributes().getClef().getSign();
 		   //Draw the Music lines on the GUI
 		      	int y = 0;
 		      	double limit = getlimit();
 	    		
-		      	//Checking the instrument name
 		    	if(instName.equals("Guitar")) {
-		    		//Setting the y axis for the notes to 0
 		    		y = 0;
-		    		//Running the loop for maximum number of notes
-		    		for(int i=0;i<notes.size();i++) {
-						int yy = notes.get(i).getNotations().getTechnical().getString();
+					int counter=1;
 
-						int f=0;
-						
-						//Checking if Grace is null
-						if(notes.get(i).getGrace()!=null) {
-							//If not null, setting the font size to 11
-							f=11;
-						}
-						else {
-							//If null, setting the font size to 14
-							f=14;
-
-						}
-						//Setting the x-axis
-						double xpos=NoteInfo.notePos(notes,notes.get(i));
-						//Setting the y-axis of the note to the string number and multiplying it with 13 because 13
-						//units is the difference between the drawn lines on the screen
-						y=0+(yy-1)*13;
-						
-						//Drawing the notes by passing the x, y and the font size
-						drawNotes(xpos, y+5,String.valueOf(notes.get(i).getNotations().getTechnical().getFret()),f);
-						
-					}
-		    	}
-		    	//Checking the instrument name
-		    	else if(instName.equals("Bass")) {
-		    		y = 0;
-		    		
-		    		//Running the loop for maximum number of notes
 		    		for(int i=0;i<notes.size();i++) {
 						int yy = notes.get(i).getNotations().getTechnical().getString();
 						int x;
 						int f=0;
-				
+					//	System.out.println(notes.get(i).getChord()!=null);
+					//	System.out.println(counter);
 						if(notes.get(i).getGrace()!=null) {
-							//If not null, setting the font size to 11
 							f=11;
+						//	System.out.println("ran");
+							
 						}
 						else {
-							//If null, setting the font size to 14
 							f=14;
+
 						}
-						//Setting the x-axis
 						double xpos=NoteInfo.notePos(notes,notes.get(i));
-						//Setting the y-axis of the note to the string number and multiplying it with 13 because 13
-						//units is the difference between the drawn lines on the screen
 						y=0+(yy-1)*13;
 						
-						//Drawing the notes by passing the x, y and the font size
 						drawNotes(xpos, y+5,String.valueOf(notes.get(i).getNotations().getTechnical().getFret()),f);
 						
 					}
 		    	}
-		    	//Checking the instrument name
 		    	else if(instName.equals("Drumset")) {
 
 		    		int x = 50;
-		    		int y2 = 0;	    	
+		    		int y2 = 0;
+//		    		y = 0;
 		    		int count = 50;
+//		    		int x2 = x;
 
-		    		
 		    		for(int i = 0; i < notes.size(); i++) {
 		    		if(notes.get(i).getNotehead() != null) {
 		    			if(notes.get(i).getInstrument().getId().equals("P1-I50")) {
@@ -303,51 +284,38 @@ public class PreviewMXLController {
 		    				drawVerticalLines(x+5, y);
 		    			}
 		    			
-		    				count = x;
-		    		
+		    			count = x;
+//		    			System.out.println(notes.get(i).getInstrument());
 		    			x+=30;
 		    		}
 		    		
 		    		else {
-		    			
 		    			if(notes.get(i).getUnpitched().getDisplaystep().equals("C")) {
 		    				y2 = 20;
 		    			}
 		    			else if(notes.get(i).getUnpitched().getDisplaystep().equals("F")){
 		    				y2 = 45;
 		    			}
-		    			else if(notes.get(i).getUnpitched().getDisplaystep().equals("D")) {
-		    				y2 = 13;
-		    			}
-		    			else if(notes.get(i).getUnpitched().getDisplaystep().equals("E")) {
-		    				y2 = 7;
-		    			}
-
-		    			drawCircle(count, y2);
-		    			drawVerticalLines(count +5, y2);
-		    	
-		    			count+=30;
+//		    			if(notes.get(i).getInstrument().getId().equals("P1-I36")) {
+//		    				drawCircle(count + 10, y2);
+//		    			}
+//		    			else {
+		    				drawCircle(count, y2);
+			    			drawVerticalLines(count +5, y2);
+//		    			}
+		    			
+		    			//drawNotes(count, y2,String.valueOf(notes.get(i).getNotations().getTechnical().getFret()));
+		    			count+=20;
 		    		}
 		    		}
 		    	}
 		    	for (int i = 1; i <= limit; i++) {
-
-
-		      		instrumentMusicLines(instName, y);
-		      		//Draw Clef
-
-		      		System.out.println("run");
-
-
-
 		    		y=0;
 		    		instrumentMusicLines(instName, y);
 		      		//Draw TAB
 
-
 		      		instrumentMusicLines(instName, y);
 		      		//Draw Clef
-
 		        	drawClef(cleff, 6, 20+y);
 
 		        	//Draw Bar lines
@@ -366,9 +334,4 @@ public class PreviewMXLController {
 		} 
 
     }
-
-
-
 }
-
-
